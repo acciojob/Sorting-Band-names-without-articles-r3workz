b7b913d2fbfa53d3fa10f91805927b764dd37aa4
r3workz <122603849+r3workz@users.noreply.github.com>
2023-09-12 19:33:12 +0000

    attempt-test

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 //your code here
 
+let touristSpots = ['Victoria Memorial','The Virupaksha Temple', 'Tajmahal'];
+
+touristSpots.sort((a,b)=>{
+    const splittedA = a.split(" ")
+    const splittedB = b.split(" ")
+    if(splittedA[0].toLowerCase() ==="a"||splittedA[0].toLowerCase()==="an"||splittedA[0].toLowerCase()==="the"){
+        // console.log("A",a)
+        return splittedA[1].toLowerCase().localeCompare(b.toLowerCase())
+    }
+    if(splittedB[0].toLowerCase() ==="a"||splittedB[0].toLowerCase()==="an"||splittedB[0].toLowerCase()==="the"){
+        // console.log("B",b)
+        return a.toLowerCase().localeCompare(splittedB[1].toLowerCase())
+        
+    }
+    return a.toLowerCase().localeCompare(b.toLowerCase())
+})
